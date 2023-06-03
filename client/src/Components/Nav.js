d5f1c7bebb576362a49233c955fefe9e4c97f0a0
Bryan <bryanpablo472@gmail.com>
2023-06-03 14:39:14 -0700

    navbar complete and hero section

---
+++ client/src/Components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+    <nav class="navbar">
+        <div class="container-fluid">
+        <a class="navbar-name">Bryan Pablo</a>
+         <ul className='nav-list'>
+            <li>projects</li>
+            <li>About</li>
+            <li>Contact</li>
+         </ul>
+        </div>
+    </nav>
+       
+    );
+}
+
+export default Nav;
