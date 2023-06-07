a470a6bea1bd3fb41301a7777f351aacf724ecb0
Bryan <bryanpablo472@gmail.com>
2023-06-06 19:15:52 -0700

    Added homepage in components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
 import './App.css';
 import React from 'react';
-import Nav from './Components/Nav';
-import {BrowserRouter} from 'react-router-dom';
-import Hero from './Components/Hero';
-import Projects from './Components/Projects';
-import LanguagesTools from './Components/LanguagesTools';
-import About from './Components/About';
-import Contact from './Components/Contact';
+import HomePage from './Components/HomePage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
-      <Nav/>
-      <Hero/>
-      <Projects/>
-      <LanguagesTools/>
-      <About/>
-      <Contact/>
-      
-      </BrowserRouter>
+        <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
